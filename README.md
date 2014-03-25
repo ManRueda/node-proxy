8ee1773bab3c341375ac065cc2e76ab4379380df
Manuel Rueda <manuel.rueda.un@gmail.com>
2014-03-24 21:29:48 -0300

    Original code & README

---
+++ README.md
@@ -0,0 +1,35 @@
+node-proxy
+=========
+---
+node-proxy es una simple implementación de un proxy para hacer llamadar por JSONP a recursos que están fuera del dominio (evitar [Same-origin policy] [Same-origin]).
+
+##### Parámetros (GET URL Parameters)
+  - o: Deber ser "p" para utilizar el proxy.
+  - u: URL de donde se obtendrán los datos.
+  - callback: Nombre de la función de retorno de JSONP.
+
+
+Version
+----
+
+0.0.1
+
+
+Ejemplo
+--------------
+
+```sh
+http://midominio.com/?u=p&u=http://www.google.com.ar/&callback=onJSONP
+```
+
+
+
+License
+----
+
+MIT
+
+
+[Same-origin]:https://developer.mozilla.org/en-US/docs/Web/JavaScript/Same_origin_policy_for_JavaScript
+
+    
---
+++ src/app.js
@@ -0,0 +1,26 @@
+var http = require('http');
+var url = require('url');
+
+var ipaddr = process.env.IP || "127.0.0.1";
+var port = process.env.PORT || 80;
+
+http.createServer(function (req, res) {
+  var query = url.parse(req.url, true).query;
+  if (query.o === 'p') {
+    http.get(query.u, function onGet(response) {
+      var sRes = query.callback + '(';
+      response.on("data", function (chunk) {
+        sRes += chunk;
+      });
+      response.on("end", function () {
+        res.writeHead(200, {
+          "Content-Type": "application/json"
+        });
+        res.end(sRes + ');');
+      });
+    });
+  } else {
+    res.writeHead(404);
+    res.end();
+  }
+}).listen(port, ipaddr);
